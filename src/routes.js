86b6a7e797c32a1007d0fb91c170494812ad69a0
Felipe Sousa <felipesrch@gmail.com>
2020-12-08 19:08:53 -0300

    inicio página vagas

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
 import Tela_Home from './pages/Home';
 import Tela_Cadastro from './pages/Cadastro';
+import Tela_Vagas from './pages/Vagas';
 
 export default function Routes() {
     return (
@@ -11,6 +12,7 @@ export default function Routes() {
           
           <Route path="/" exact component={Tela_Home} />
           <Route path="/cadastro" exact component={Tela_Cadastro} />
+          <Route path="/vagas" exact component={Tela_Vagas} />
   
         </Switch>
       </BrowserRouter>
